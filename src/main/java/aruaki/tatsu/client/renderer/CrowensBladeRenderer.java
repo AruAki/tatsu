5aac13baa90c1ec6236ae955b433c14b1f51564e
David Hiebert <dhiebert2013@gmail.com>
2024-11-21 11:27:33 -0700

    Glowmask Layer + remove texture from inside model

--- src/main/java/aruaki/tatsu/client/renderer/CrowensBladeRenderer.java
+++ src/main/java/aruaki/tatsu/client/renderer/CrowensBladeRenderer.java
@@ -5,9 +5,11 @@ import aruaki.tatsu.items.CrowensBlade;
 import net.minecraft.util.Identifier;
 import software.bernie.geckolib.model.DefaultedItemGeoModel;
 import software.bernie.geckolib.renderer.GeoItemRenderer;
+import software.bernie.geckolib.renderer.layer.AutoGlowingGeoLayer;
 
 public class CrowensBladeRenderer extends GeoItemRenderer<CrowensBlade> {
   public CrowensBladeRenderer() {
     super(new DefaultedItemGeoModel<>(new Identifier(Tatsu.MOD_ID, "crowens_blade")));
+    addRenderLayer(new AutoGlowingGeoLayer<>(this));
   }
 }
